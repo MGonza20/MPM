13b5361a6bf90e529f9df915dbb663e8e2516a9b
MGonza20 <sara.paguaga@gmail.com>
2022-10-10 14:00:23 -0600

    generating test for passwords

--- src/__tests__/Register.test.js
+++ src/__tests__/Register.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import Register from '../pages/Register'
 
@@ -20,5 +20,24 @@ describe('Register Component Testings', () => {
         expect(emailInput).toBeInTheDocument()
         expect(passwordInput).toBeInTheDocument()
         expect(password2Input).toBeInTheDocument()
+    }) 
+
+    test('Not matching passwords', () => {
+        render(<Register />);
+        const passwordInput = screen.getByTestId('password-input-test')
+        const password2Input = screen.getByTestId('password-veri-input-test')
+        const passwordEq = screen.getByTestId('password-test')
+
+        fireEvent.change(passwordInput, {
+            persist: jest.fn(),
+            target: { name: "password", value: "123"}
+        });
+
+        fireEvent.change(password2Input, {
+            persist: jest.fn(),
+            target: { name: "password", value: "12"}
+        });
+
+        expect(passwordEq.value).toEqual("not match");
     }) 
 })
