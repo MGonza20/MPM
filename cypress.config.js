2506f3b6eee10a7b0d870be3ca0f633dc38a71d1
Cristian Laynez <54615310+CRLB-sketch@users.noreply.github.com>
2022-11-06 15:30:06 -0800

    Test Login Complete

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,20 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-    e2e: {
-        setupNodeEvents(on, config) {
-            // implement node event listeners here
-        },
-        specPattern: [
-            'cypress/integration/webUI/*.js',
-            'cypress/integration/examples/*.js',
-        ],
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
     },
-})
+    specPattern: [
+      "cypress/integration/webUI/*.js",
+      "cypress/integration/examples/*.js",
+    ],
+  },
+
+  component: {
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
+    },
+  },
+});
--- cypress/integration/webUI/LoginTest.js
+++ cypress/integration/webUI/LoginTest.js
@@ -2,25 +2,25 @@
 // cypress - Spec
 
 describe('My Pet & Me - Login Test', function () {
-    // it('Test of Invalid User', function () {
-    //     cy.visit('http://localhost:3000/')
+    it('Test of Invalid User', function () {
+        cy.visit('http://localhost:3000/')
 
-    //     cy.wait(1000)
+        cy.wait(1000)
 
-    //     cy.get(':nth-child(3) > a').click()
+        cy.get(':nth-child(3) > a').click()
 
-    //     cy.wait(1000)
+        cy.wait(1000)
 
-    //     // Probar cuenta invalida
-    //     cy.get(':nth-child(1) > .chakra-input').type(
-    //         'soy_un_colado@facke.gmail'
-    //     )
-    //     cy.get(':nth-child(2) > .chakra-input').type('contrafake')
+        // Probar cuenta invalida
+        cy.get(':nth-child(1) > .chakra-input').type(
+            'soy_un_colado@facke.gmail'
+        )
+        cy.get(':nth-child(2) > .chakra-input').type('contrafake')
 
-    //     cy.get('.chakra-button').click()
+        cy.get('.chakra-button').click()
 
-    //     cy.wait(5000) // Solamente para ver el error que puede ocurrir
-    // })
+        cy.wait(5000) // Solamente para ver el error que puede ocurrir
+    })
 
     it('Test of Existing User', function () {
         cy.visit('http://localhost:3000/')
@@ -39,43 +39,71 @@ describe('My Pet & Me - Login Test', function () {
 
         // -----------------------------------------------------------------------------------
         // Como ya tenemos una veterinaria ya registrada entonces vamos a ir de una vez ------
-        cy.get('#field-\\:r0\\:').type('Veterinaria Cypress 1') // Nombre
+        cy.get('#field-\\:r0\\:').type('Veterinaria Cypress 2') // Nombre
         cy.get('#field-\\:r1\\:').type('Guatemala') // Ciudad
         cy.get('#field-\\:r2\\:').type('15') // Zona
         cy.get('#field-\\:r3\\:').type('Cayala') // Dirección
-        cy.get('#field-\\:r4\\:').type('vetcypress1@gmail.com') // Correo
+        cy.get('#field-\\:r4\\:').type('vetcypress2@gmail.com') // Correo
 
         // Servicios ofrecidos : [data-testid="handle-add-vet-test"] > :nth-child(6)
         // Vacunacion : [data-testid="vacunacion-test"] > .chakra-checkbox__label
-        // Rayos X : [data-testid="rayos-x-test"] > .chakra-checkbox__label
-        // Examenes corporales : [data-testid="examenes-corporales-test"] > .chakra-checkbox__label
+        cy.get(
+            '[data-testid="vacunacion-test"] > .chakra-checkbox__label'
+        ).click()
+        // RayosX : [data-testid="rayos-x-test"] > .chakra-checkbox__label
+        cy.get('[data-testid="rayos-x-test"] > .chakra-checkbox__label').click()
+        // Examenescorporales : [data-testid="examenes-corporales-test"] > .chakra-checkbox__label
+        cy.get(
+            '[data-testid="examenes-corporales-test"] > .chakra-checkbox__label'
+        ).click()
         // Hematologías : [data-testid="hematologias-test"] > .chakra-checkbox__label
+        cy.get(
+            '[data-testid="hematologias-test"] > .chakra-checkbox__label'
+        ).click()
+
+        // ! --------------------------------------------------------------------------
+        // ! Ejjj....................................................................--
         // Hospedaje : [data-testid="hospedaje-test"] > .chakra-checkbox__label
+        // cy.get(
+        //     '[data-testid="hospedaje-test"] > .chakra-checkbox__label'
+        // ).click()
         // Grooming : [data-testid="grooming-test"] > .chakra-checkbox__label
+        // cy.get(
+        // '[data-testid="grooming-test"] > .chakra-checkbox__label'
+        // ).click()
         // Desparacitacion : [data-testid="desparacitacion-test"] > .chakra-checkbox__label
+        // cy.get(
+        // '[data-testid="desparacitacion-test"] > .chakra-checkbox__label'
+        // ).click()
         // Castraciones : [data-testid="castraciones-test"] > .chakra-checkbox__label
+        // cy.get(
+        // '[data-testid="castraciones-test"] > .chakra-checkbox__label'
+        // ).click()
         // Operacion : [data-testid="operacion-test"] > .chakra-checkbox__label
-
-        // cy.get('[type="checkbox"]').check() // Check checkbox element
-        // cy.get('[type="radio"]').first().check()
-
-        // cy.get('[type="checkbox"]').check() // Check checkbox element
-
-        // Seleccionar ubicacion en el mapa
+        // cy.get(
+        // '[data-testid="operacion-test"] > .chakra-checkbox__label'
+        // ).click()
+        // ! --------------------------------------------------------------------------
 
         cy.get('#field-\\:r5\\:').type('9999-0001') // Telefono
 
-        // Emergencia : .chakra-radio-group > .chakra-stack
-        // cy.get(".chakra-radio-group > .chakra-stack > :nth-child(1)") // Si
-        // cy.get('.chakra-radio-group > .chakra-stack > :nth-child(2)').check() // No
+        // 1) Si, 2) No
+        cy.get('.chakra-radio-group > .chakra-stack > :nth-child(1)').click()
 
-        // Tipo de veterinaria : #field-\:ra\:
+        /**
+         * 1. Normal
+         * 2. PhetShop
+         * 3. Clinica
+         * 4. Hospital
+         * Tipo de veterinaria : #field-\:ra\:
+         */
+        cy.get('#field-\\:ra\\:').select(2)
 
         // Hora de apertura : #field-\:rb\:
-        cy.get('#field-\\:rb\\:').type('08:08')
+        cy.get('#field-\\:rb\\:').type('08:00')
 
         // Hora de cierre : #field-\:rc\:
-        cy.get('#field-\\:rc\\:').type('12:12')
+        cy.get('#field-\\:rc\\:').type('20:00')
 
         cy.wait(2000)
 
