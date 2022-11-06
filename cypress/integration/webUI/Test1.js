539a154eb34a988dac2f8621f1d1e22d15f89acf
MGonza20 <sara.paguaga@gmail.com>
2022-11-05 22:37:39 -0600

    trying to do test

--- cypress/integration/webUI/Test1.js
+++ cypress/integration/webUI/Test1.js
@@ -1,3 +1,9 @@
 /* eslint-disable no-undef */
 // cypress - Spec 
 
+describe('My first Test Suite', function()
+{
+    it('My FirstTest case', function(){
+        cy.visit("'http://localhost:3000/%27")
+    })
+})
