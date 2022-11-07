f5aed50771f318c453eb20d1a2fc824faa00c62b
alv18051 <alv18051@uvg.edu.gt>
2022-11-06 19:11:37 -0600

    adding delay

--- cypress/integration/webUI/NavigationTest.js
+++ cypress/integration/webUI/NavigationTest.js
@@ -11,7 +11,9 @@ describe("Renders the project, verifies you can navigate", () => {
     });
 
     it("Renders FAQ page", () => {
+        cy.wait(1000);
         cy.get(':nth-child(4) > a').click();
+        cy.wait(1000);
         cy.get(':nth-child(4) > a').click();
         cy.url().should("include", "/faq");
         cy.findByText("Preguntas Frecuentes").should("exist");
@@ -19,10 +21,15 @@ describe("Renders the project, verifies you can navigate", () => {
 
     it("Renders the search page, goes to a vet, verifies popup, back to homepage", () => {
         cy.get("h3").should("contain", "Brindandote tranquilidad a ti y bienestar a tu mascota.");
+        cy.wait(1000);
         cy.get('[data-testid="search"] > a').click();
+        cy.wait(1000);
         cy.url().should("include", "/search");
+        cy.wait(1000);
         cy.get(':nth-child(1) > [data-testid="card-component-test"] > .chakra-stack > [data-testid="image-card-test"]').click();
+        cy.wait(1000);
         cy.get("h1").should("contain", "Correo");
+        cy.wait(100);
         cy.get('b').click();
         cy.get("h2").should("contain", "My Pet & Me");
         
