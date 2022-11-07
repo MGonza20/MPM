240e291c5f58121d2263c4ca28eb5f893a216cfb
alv18051 <alv18051@uvg.edu.gt>
2022-11-06 18:23:09 -0600

    update tests to be more concise

--- cypress/integration/webUI/HomePage.js
+++ cypress/integration/webUI/HomePage.js
@@ -5,12 +5,23 @@ describe("Renders the home page", () => {
         cy.get("h2").should("contain", "My Pet & Me");
     });
 
-    it("Renders the home page, goes to a vet", () => {
+    it("Renders FAQ page", () => {
+        cy.visit("/"); 
+        cy.get(':nth-child(4) > a').click();
+        cy.get(':nth-child(4) > a').click();
+        cy.get("h1").should("contain", "1. ¿Cómo debo usar la página en caso de una emergencia?");
+    });
+
+    it("Renders the search page, goes to a vet, verifies popup, back to homepage", () => {
         cy.visit("/");
         cy.get("h3").should("contain", "Brindandote tranquilidad a ti y bienestar a tu mascota.");
         cy.get('[data-testid="search"] > a').click();
         cy.get(':nth-child(1) > [data-testid="card-component-test"] > .chakra-stack > [data-testid="image-card-test"]').click();
         cy.get("h1").should("contain", "Correo");
+        cy.get('b').click();
+        cy.get("h2").should("contain", "My Pet & Me");
         
     });
+
+    
 });
