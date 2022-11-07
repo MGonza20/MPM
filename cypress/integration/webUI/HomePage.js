514b167871f613e2253f87a3e6a20c4af9b09077
alv18051 <alv18051@uvg.edu.gt>
2022-11-06 18:52:30 -0600

    Update HomePage.js

--- cypress/integration/webUI/HomePage.js
+++ cypress/integration/webUI/HomePage.js
@@ -1,19 +1,22 @@
 /* eslint-disable no-undef */
 describe("Renders the home page", () => {
-    it("Renders the home page", () => {
+
+    beforeEach(() => {
         cy.visit("/");
+    });
+
+    it("Renders the home page", () => {
         cy.get("h2").should("contain", "My Pet & Me");
+        cy.findByText("Brindandote tranquilidad a ti y bienestar a tu mascota.").should("exist");
     });
 
     it("Renders FAQ page", () => {
-        cy.visit("/"); 
         cy.get(':nth-child(4) > a').click();
         cy.get(':nth-child(4) > a').click();
         cy.findByText("Preguntas Frecuentes").should("exist");
     });
 
     it("Renders the search page, goes to a vet, verifies popup, back to homepage", () => {
-        cy.visit("/");
         cy.get("h3").should("contain", "Brindandote tranquilidad a ti y bienestar a tu mascota.");
         cy.get('[data-testid="search"] > a').click();
         cy.get(':nth-child(1) > [data-testid="card-component-test"] > .chakra-stack > [data-testid="image-card-test"]').click();
