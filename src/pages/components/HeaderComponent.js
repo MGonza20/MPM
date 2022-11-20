2bc4deddf585feb1f132fe8f9996092ec5fb7676
Cristian Laynez <54615310+CRLB-sketch@users.noreply.github.com>
2022-11-20 14:47:19 -0800

    Fixing Log

--- src/pages/components/HeaderComponent.js
+++ src/pages/components/HeaderComponent.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/react-in-jsx-scope */
 /**#######################################################################################
  * Universidad del Valle de Guatemala
  * Departamento de Ciencias de la Computación
@@ -14,71 +15,70 @@
  * Javier Alvarez
  #######################################################################################*/
 
+// import '../styles/index.css'
+// import PropTypes from 'prop-types'
+
+/* eslint-disable linebreak-style */
+
 import { Link, useNavigate } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import { logout, reset } from '../../features/auth/authSlice'
 
-import onLogout from '../functions/OnLogout'
-
 function HeaderComponent() {
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
-    const { user } = useSelector((state) => state.auth)
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const { user } = useSelector((state) => state.auth)
 
-    return (
-        <>
-            <nav className="header" data-testid={'header-test'}>
-                <Link to={'/'}>
-                    {' '}
-                    <h1 className="logo">
-                        <b>My Pet&Me</b>
-                    </h1>{' '}
-                </Link>
+  const onLogout = () => {
+    dispatch(logout())
+    dispatch(reset())
+    navigate('/')
+  }
 
-                <ul className="header-right">
-                    {user ? (
-                        <>
-                            <li className="option btn">
-                                <Link to={'/RegisterVet'}>Formulario</Link>
-                            </li>
+  return (
+    <>
+      <nav className="header">
+        <Link to={'/'}>
+          {' '}
+          <h1 className="logo">
+            <b>My Pet&Me</b>
+          </h1>{' '}
+        </Link>
 
-                            <li className="option btn">
-                                <button
-                                    onClick={onLogout(
-                                        dispatch,
-                                        logout,
-                                        reset,
-                                        navigate
-                                    )}
-                                    data-testid="onLogout"
-                                >
-                                    X
-                                </button>
-                            </li>
-                        </>
-                    ) : (
-                        <>
-                            <li className="option active btn">
-                                <Link to={'/emergency'}>Emergencia</Link>
-                            </li>
+        <ul className="header-right">
+          {user ? (
+            <>
+              <li className="option btn">
+                <Link to={'/RegisterVet'}>Formulario</Link>
+              </li>
 
-                            <li className="option btn" data-testid="search">
-                                <Link to={'/search'}>Búsqueda</Link>
-                            </li>
+              <li className="option btn">
+                <button onClick={onLogout}>X</button>
+              </li>
+            </>
+          ) : (
+            <>
+              <li className="option active btn">
+                <Link to={'/emergency'}>Emergencia</Link>
+              </li>
 
-                            <li className="option btn">
-                                <Link to={'/login'}>Iniciar sesión</Link>
-                            </li>
+              <li className="option btn" data-testid="search">
+                <Link to={'/search'}>Búsqueda</Link>
+              </li>
 
-                            <li className="option btn">
-                                <Link to={'/faq'}>FAQ</Link>
-                            </li>
-                        </>
-                    )}
-                </ul>
-            </nav>
-        </>
-    )
+              <li className="option btn">
+                <Link to={'/login'}>Iniciar sesión</Link>
+              </li>
+
+              <li className="option btn">
+                <Link to={'/faq'}>FAQ</Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </nav>
+    </>
+  )
 }
 
 export default HeaderComponent
