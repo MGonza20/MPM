618293c91c3412808e9aba0830ae83432b985241
MGonza20 <sara.paguaga@gmail.com>
2022-11-20 18:39:33 -0600

    fix

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const colors = require('colors')
-const dotenv = require('dotenv').config()
+const dotenv = require('dotenv')
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
+
+dotenv.config()
 const port = process.env.PORTMDB || 5000
 
 const bodyParser = require('body-parser')
