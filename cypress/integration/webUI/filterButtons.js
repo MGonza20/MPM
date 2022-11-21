0a9b3aa1394e8e89df16d3ba7cbfc1e04c2e279e
diego6ruiz <55885621+diego6ruiz@users.noreply.github.com>
2022-11-20 18:26:37 -0600

    Update filterButtons.js
    Filtro de vets con servicio emergencia/filtro por tipo de vet

--- cypress/integration/webUI/filterButtons.js
+++ cypress/integration/webUI/filterButtons.js
@@ -15,6 +15,30 @@ describe("Testing the buttons on our search page", () => {
         cy.get(':nth-child(1) > .chakra-select__wrapper > .chakra-select').select('false');
         cy.get('form > .chakra-button').click();
         cy.findByText("Sara Tech Maxi Vet 2").should("exist");
-    });   
+    });
+    
+    it("Search emergency Si ", () => {
+        cy.get("h2").should("contain", "My Pet & Me");
+        cy.findByText("Brindandote tranquilidad a ti y bienestar a tu mascota.").should("exist");
+        cy.get('[data-testid="search"] > a').click();
+        cy.wait(1000);
+        cy.get('form > .chakra-button').click();
+        cy.get('form > .chakra-button').click();
+        cy.findByText("El Vet JAJA").should("exist");
+    }); 
+    
 
+    it("Search by petshop", () => {
+        cy.get("h2").should("contain", "My Pet & Me");
+        cy.findByText("Brindandote tranquilidad a ti y bienestar a tu mascota.").should("exist");
+        cy.get('[data-testid="search"] > a').click();
+        cy.wait(1000);
+        cy.get(':nth-child(1) > .chakra-select__wrapper > .chakra-select').select('false');
+        cy.get(':nth-child(3) > .chakra-select__wrapper > .chakra-select').select('Petshop');
+        cy.get('form > .chakra-button').click();
+        cy.get(':nth-child(1) > .chakra-select__wrapper > .chakra-select').select('false');
+        cy.get(':nth-child(3) > .chakra-select__wrapper > .chakra-select').select('Petshop');
+        cy.get('form > .chakra-button').click();
+        cy.findByText("Pet Shop San Carlos").should("exist");
+    });
 });
